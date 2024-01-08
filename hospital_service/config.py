1bf55baecf404468343fa07c68ba6ac41ef8f228
Cosmin <cosmin.catalin270@gmail.com>
2024-01-08 11:05:45 +0200

    Update config file and remove folder

---
+++ hospital_service/config.py
@@ -0,0 +1,5 @@
+import secrets
+SECRET_KEY = secrets.token_urlsafe(32)
+MARIADB_USERS_ROOT_PASSWORD = 123456789
+DATABASE_URL = f"mysql+pymysql://root:{MARIADB_USERS_ROOT_PASSWORD}@mariadb/hospital_db"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
