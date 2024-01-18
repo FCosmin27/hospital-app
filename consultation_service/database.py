27d8fbc15fff7a5f36ba574d6a36c6fc3c9384b5
Cosmin <cosmin.catalin270@gmail.com>
2024-01-18 20:58:43 +0200

    Add init_db, create_users functions

--- consultation_service/database.py
+++ consultation_service/database.py
@@ -1,4 +1,44 @@
 from mongoengine import connect
-from config import MONGODB_URI
+from pymongo.errors import OperationFailure
+from config import (
+    MONGODB_URI,
+    MONGODB_CONSULTATION_ROOT_USERNAME,
+    MONGODB_CONSULTATION_ROOT_PASSWORD,
+    MONGODB_CONSULTATION_DB_NAME
+)
 
-connect(host=MONGODB_URI)
+def init_db():
+    connection = connect(
+        db=MONGODB_CONSULTATION_DB_NAME,
+        username=MONGODB_CONSULTATION_ROOT_USERNAME,
+        password=MONGODB_CONSULTATION_ROOT_PASSWORD,
+        host=MONGODB_URI
+    )
+
+def create_users():
+    connection = connect(
+        db=MONGODB_CONSULTATION_DB_NAME,
+        username=MONGODB_CONSULTATION_ROOT_USERNAME,
+        password=MONGODB_CONSULTATION_ROOT_PASSWORD,
+        host=MONGODB_URI
+    )
+     
+    db = connection[MONGODB_CONSULTATION_DB_NAME]
+
+    try:
+        db.command(f"createUser", "doctor",
+                   pwd="{MONGODB_CONSULTATION_ROOT_PASSWORD}",
+                   roles=["readWrite"])
+        print("Doctor user created.")
+    except OperationFailure as e:
+        print(f"Doctor user creation failed: {e}")
+
+    try:
+        db.command(f"createUser", "patient",
+                   pwd="{MONGODB_CONSULTATION_ROOT_PASSWORD}",
+                   roles=["read"])
+        print("Patient user created.")
+    except OperationFailure as e:
+        print(f"Patient user creation failed: {e}")
+
+    return connection
