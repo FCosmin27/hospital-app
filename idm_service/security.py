bd237eb54af61bc145eeca89fbc4f7d9bd980fff
Cosmin <cosmin.catalin270@gmail.com>
2024-01-20 23:44:57 +0200

    Secure endpoints using JWT

--- idm_service/security.py
+++ idm_service/security.py
@@ -1,23 +1,21 @@
-from passlib.context import CryptContext
-from jose import jwt
+from jose import JWTError, jwt
 from datetime import datetime, timedelta
-from jose.exceptions import JWTError
-from .config import SECRET_KEY
+from config import SECRET_KEY, ACCESS_TOKEN_EXPIRE_MINUTES, ALGORITHM
+from crypto.decrypt import check_password_hash
+from crypto.encrypt import generate_password_hash
 
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-def create_access_token(data: dict, expires_delta: timedelta = None):
+def create_access_token(data: dict):
     to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+def verify_password(plain_password, hashed_password):
+    return check_password_hash(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return generate_password_hash(password)
 
 def verify_token(token: str, credentials_exception):
     try:
@@ -25,7 +23,6 @@ def verify_token(token: str, credentials_exception):
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
-        # token_data = TokenData(username=username) # Ensure you have a TokenData model
+        return payload
     except JWTError:
         raise credentials_exception
-    # return token_data
