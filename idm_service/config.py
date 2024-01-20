19085ba13e6287cece9778803b6b55596910b12b
Cosmin <cosmin.catalin270@gmail.com>
2024-01-20 21:46:40 +0200

    Correct config file

--- idm_service/config.py
+++ idm_service/config.py
@@ -1,4 +1,5 @@
-SECRET_KEY = "YOUR_SECRET_KEY"
+import secrets
+SECRET_KEY = secrets.token_urlsafe(32)
 MARIADB_USERS_ROOT_PASSWORD = 123456789
-DATABASE_URL = f"mysql+pymysql://root:{MARIADB_USERS_ROOT_PASSWORD}@mariadb/users_db"
+DATABASE_URL = f"mysql+pymysql://root:{MARIADB_USERS_ROOT_PASSWORD}@mariadb_users/users_db"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
