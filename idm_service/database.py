1790087adc3eb5eb066510a0a8176727b79ae97f
Cosmin <cosmin.catalin270@gmail.com>
2024-01-21 13:24:13 +0200

    Moved files to correct files and add __init__.py

--- idm_service/database.py
+++ idm_service/database.py
@@ -1,9 +1,43 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, inspect
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from .config import DATABASE_URL
+from config import DATABASE_URL
+import models
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def delete_tables():
+    inspector = inspect(engine)
+
+    if inspector.has_table('user_roles'):
+        models.user_roles.drop(engine)
+
+    if inspector.has_table('users'):
+        models.Users.__table__.drop(engine)
+    
+    if inspector.has_table('roles'):
+        models.Roles.__table__.drop(engine)
+
+def insert_roles_into_table():
+    roles_to_insert = ['admin', 'doctor', 'patient']
+    
+    with SessionLocal() as db:
+        existing_roles = db.query(models.Roles).all()
+        existing_roles_names = [role.name for role in existing_roles]
+        
+        for role in roles_to_insert:
+            if role not in existing_roles_names:
+                db.add(models.Roles(name=role))
+                
+        db.commit()
+        db.close()
--- idm_service/dependencies.py
+++ idm_service/dependencies.py
@@ -2,17 +2,10 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from security import verify_token
 from sqlalchemy.orm import Session
-from database import SessionLocal
+from database import get_db
 import models
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
@@ -24,7 +17,7 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
 
 def check_current_user_is_admin(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     current_user = get_current_user(token)
-    db_user = db.query(models.User).filter(models.User.id == current_user.id).first()
+    db_user = db.query(models.User).filter(models.Users.id == current_user.id).first()
     
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
