ce2d52c8986966528c627dbc57d1db034c3463d2
Cosmin <cosmin.catalin270@gmail.com>
2024-01-21 11:29:15 +0200

    Changes to crud methods and to config

--- idm_service/crud.py
+++ idm_service/crud.py
@@ -1,32 +1,41 @@
 from sqlalchemy.orm import Session
-from . import models, schemas
-from .security import get_password_hash
+import models, schemas, security
+from fastapi import HTTPException
 
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
 
-def get_user_by_email(db: Session, email: str):
-    return db.query(models.User).filter(models.User.email == email).first()
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
-def create_user(db: Session, user: schemas.UserCreate):
-    hashed_password = get_password_hash(user.password)
-    db_user = models.User(email=user.email, hashed_password=hashed_password)
-    db.add(db_user)
+def create_user(db: Session, user: schemas.UserCreate, role_name: str):
+    hashed_password = security.generate_password_hash(user.password)
+    new_user = models.User(username=user.username, hashed_password=hashed_password)
+    role = db.query(models.Role).filter(models.Role.name == role_name).first()
+    if not role:
+        raise HTTPException(status_code=404, detail=f"Role '{role_name}' not found")
+    new_user.roles.append(role)
+    db.add(new_user)
     db.commit()
-    db.refresh(db_user)
-    return db_user
+    db.refresh(new_user)
+    return new_user
+
 
 def update_user(db: Session, user_id: int, user: schemas.UserUpdate):
     db_user = db.query(models.User).filter(models.User.id == user_id).first()
-    if user.username:
-        db_user.username = user.username
-    if user.email:
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    if user.email is not None:
         db_user.email = user.email
-    if user.password:
-        db_user.hashed_password = get_password_hash(user.password)
+    if user.password is not None:
+        db_user.hashed_password = security.get_password_hash(user.password)
+    if user.is_active is not None:
+        db_user.is_active = user.is_active
+ 
     db.commit()
     db.refresh(db_user)
     return db_user
