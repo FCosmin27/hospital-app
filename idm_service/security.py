3223472051fec593cbdc3c712124a990fcb04dbc
Cosmin <cosmin.catalin270@gmail.com>
2024-01-21 16:01:10 +0200

    Move methods from dependencies to security and delete unncesessary methods

--- idm_service/security.py
+++ idm_service/security.py
@@ -1,8 +1,34 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from config import SECRET_KEY, ACCESS_TOKEN_EXPIRE_MINUTES, ALGORITHM
-from crypto.decrypt import check_password_hash
-from crypto.encrypt import generate_password_hash
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from database import get_db
+import models
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    return verify_token(token, credentials_exception)
+
+def check_current_user_is_admin(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    db_user = verify_token(token, credentials_exception, db)
+    
+    if any(role.name == 'admin' for role in db_user.roles):
+        return db_user
+    else:
+        raise HTTPException(status_code=403, detail="Access denied")
 
 def create_access_token(data: dict):
     to_encode = data.copy()
@@ -11,18 +37,15 @@ def create_access_token(data: dict):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-def verify_password(plain_password, hashed_password):
-    return check_password_hash(plain_password, hashed_password)
-
-def get_password_hash(password):
-    return generate_password_hash(password)
-
-def verify_token(token: str, credentials_exception):
+def verify_token(token: str, credentials_exception, db: Session):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
-        return payload
+        user = db.query(models.Users).filter(models.Users.username == username).first()
+        if user is None:
+            raise credentials_exception
+        return user
     except JWTError:
         raise credentials_exception
